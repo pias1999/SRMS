22fdc386d1c776fcfaea4979f25c78bee2ad7766
Pias <ashikuzzaman.pias.cse@ulab.edu.bd>
2022-01-06 21:23:25 +0600

    initial commit

---
+++ app/src/main/java/com/example/srms/StBasicInfo.java
@@ -0,0 +1,71 @@
+package com.example.srms;
+
+public class StBasicInfo {
+
+        public String fullname;
+        public String dept;
+        public String fathersname;
+        public String mothersname;
+        public String guardcont;
+        public String studcont;
+
+        public StBasicInfo() {
+        }
+
+        public StBasicInfo(String fullname, String dept, String fathersname, String mothersname, String guardcont, String studcont) {
+            this.fullname = fullname;
+            this.dept = dept;
+            this.fathersname = fathersname;
+            this.mothersname = mothersname;
+            this.guardcont = guardcont;
+            this.studcont = studcont;
+        }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getFathersname() {
+        return fathersname;
+    }
+
+    public void setFathersname(String fathersname) {
+        this.fathersname = fathersname;
+    }
+
+    public String getMothersname() {
+        return mothersname;
+    }
+
+    public void setMothersname(String mothersname) {
+        this.mothersname = mothersname;
+    }
+
+    public String getGuardcont() {
+        return guardcont;
+    }
+
+    public void setGuardcont(String guardcont) {
+        this.guardcont = guardcont;
+    }
+
+    public String getStudcont() {
+        return studcont;
+    }
+
+    public void setStudcont(String studcont) {
+        this.studcont = studcont;
+    }
+}
--- app/src/main/java/com/example/srms/StudentBasicInfoActivity.java
+++ app/src/main/java/com/example/srms/StudentBasicInfoActivity.java
@@ -2,13 +2,64 @@ package com.example.srms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StudentBasicInfoActivity extends AppCompatActivity {
+
+
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private EditText inputFullname, inputDept, inputFathersname, inputMothersname, inputGuardcont, inputStudcont;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_basic_info);
+
+        inputFullname = (EditText) findViewById(R.id.student_fullname);
+        inputDept = (EditText) findViewById(R.id.student_dept);
+        inputFathersname = (EditText) findViewById(R.id.stu_fathers_name);
+        inputMothersname = (EditText) findViewById(R.id.stu_mothers_name);
+        inputGuardcont = (EditText) findViewById(R.id.stu_guardians_contact);
+        inputStudcont = (EditText) findViewById(R.id.students_contact);
+        nextButton = (Button) findViewById(R.id.student_sign_up_next);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("StBasicInfo");
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fullname = inputFullname.getText().toString();
+                String dept = inputDept.getText().toString();
+                String fathersname = inputFathersname.getText().toString();
+                String mothersname = inputMothersname.getText().toString();
+                String guardcont = inputGuardcont.getText().toString();
+                String studcont = inputStudcont.getText().toString();
+
+                createStBasicInfo(fullname, dept, fathersname, mothersname, guardcont, studcont);
+
+                Intent i = new Intent(StudentBasicInfoActivity.this, StudentSignupActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void createStBasicInfo(String fullname, String dept, String fathersname, String mothersname, String guardcont, String studcont) {
+        String key =mFirebaseDatabase.push().getKey();
+        StBasicInfo newStudent = new StBasicInfo(inputFullname.getText().toString(), inputDept.getText().toString(),
+                inputFathersname.getText().toString(), inputMothersname.getText().toString(),
+                inputGuardcont.getText().toString(), inputStudcont.getText().toString());
+        mFirebaseDatabase.child(key).setValue(newStudent);
+        finish();
     }
 }
