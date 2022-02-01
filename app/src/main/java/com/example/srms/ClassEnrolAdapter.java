a8f2aa693ee277c6c13b088b526bb7e9f79402f6
Pias <pias1999>
2022-02-01 21:15:49 +0600

    minor changes

--- app/src/main/java/com/example/srms/ClassEnrolAdapter.java
+++ app/src/main/java/com/example/srms/ClassEnrolAdapter.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +42,22 @@ public class ClassEnrolAdapter extends RecyclerView.Adapter<MyHolder>{
         holder.vCorseCode.setText(data.getCoursecode());
         holder.vInstructor.setText(data.getInstructor());
         holder.vSection.setText(data.getSection());
+        /*holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stid = data.getStid().toString();
+                String fullname = data.getFullname().toString();
+                String dept = data.getDept().toString();
+                String fathersname = data.getFathersname().toString();
+                String mothersname = data.getMothersname().toString();
+                String guardcont = data.getGuardcont().toString();
+                String studcont = data.getStudcont().toString();
+
+                createTable(stid, fullname, dept, fathersname, mothersname, guardcont, studcont);
+
+                    Toast.makeText(getApplicationContext(), "Enrollment Successful!",
+                            Toast.LENGTH_LONG).show();
+            });*/
     }
 
     @Override
@@ -47,6 +68,7 @@ public class ClassEnrolAdapter extends RecyclerView.Adapter<MyHolder>{
 
     class MyHolder extends RecyclerView.ViewHolder{
         TextView vCorseCode , vInstructor,vSection;
+        //View mView;
 
         public MyHolder(View itemView) {
             super(itemView);
@@ -56,3 +78,12 @@ public class ClassEnrolAdapter extends RecyclerView.Adapter<MyHolder>{
 
         }
     }
+
+        /*private void createStBasicInfo(String stid, String fullname, String dept, String fathersname, String mothersname, String guardcont, String studcont) {
+            //String key =mFirebaseDatabase.push().getKey();
+            Table newStudent = new Table(data.getStid().toString(), data.getFullname().toString(), data.getDept().toString(),
+                    data.getFathersname().toString(), data.getMothersname().toString(),
+                    data.getGuardcont().toString(), data.getStudcont().toString());
+            mFirebaseDatabase.child(stid).setValue(newStudent);
+            finish();
+        }*/
